b934903f2dfb61920e6927fcd678279d9e950799
Utkarsh Upadhyay <musically.ut@gmail.com>
2015-07-12 11:32:23 +0200

    Do not throw error on pages which do not have a body tag.
    Fixes #3.

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,2 +1,6 @@
-var body = document.getElementsByTagName('body')[0];
-body.addEventListener('click', keyncognitoHandler());
+var body = document.body;
+if (body) {
+    // There are pages which do not have the body tag, e.g. links on Google
+    // search results page are to a page which only contains a script tag.
+    body.addEventListener('click', keyncognitoHandler());
+}
