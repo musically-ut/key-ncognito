2033fde2a5f51efe7443cdf03bf45b28e55b2834
Utkarsh Upadhyay <musically.ut@gmail.com>
2016-09-17 12:39:52 +0200

    Add browser action to open current Tab incognito.
    Closes #4.
    
    However, it does not remove the tab from the browser history.

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,44 +1,54 @@
 'use strict';
 
+function openIncognito(url) {
+    console.log("Attempting to open URL incognito: ", url);
+    chrome.windows.getAll({}, function (wins) {
+        var incognitoWins = wins.filter(function (w) { return w.incognito; });
+        var focussedWins = wins.filter(function (w) { return w.focused; });
+
+        var state = (focussedWins.length > 0)
+                        ? (focussedWins[0].state || "normal")
+                        : "normal";
+
+        if (incognitoWins.length > 0 && typeof incognitoWins[0].id !== "undefined") {
+            chrome.tabs.create({ url: url, windowId: incognitoWins[0].id });
+        } else {
+            try {
+                chrome.windows.create(
+                    { url: url
+                    , incognito: true
+                    // May not be able to use `state` as it is supported
+                    // only in Chrome 41 and beyond.
+                    , state: state
+                    }
+                );
+            } catch(e) {
+                console.log("Trying without `state`");
+                chrome.windows.create(
+                    { url: url
+                    , incognito: true
+                    }
+                );
+            }
+        }
+    });
+}
+
 chrome.runtime.onInstalled.addListener(function (details) {
-    if (details.reason === "install" || details.reason === "update") {
+    if (details.reason === "install") {
         chrome.runtime.openOptionsPage();
     }
 });
 
 chrome.extension.onMessage.addListener(
     function(request, sender, sendResponse) {
-        console.log("Attempting to open URL incognito: ", request.url);
-        chrome.windows.getAll({}, function (wins) {
-            var incognitoWins = wins.filter(function (w) { return w.incognito; });
-            var focussedWins = wins.filter(function (w) { return w.focused; });
-
-            var state = (focussedWins.length > 0)
-                            ? (focussedWins[0].state || "normal")
-                            : "normal";
-
-            if (incognitoWins.length > 0 && typeof incognitoWins[0].id !== "undefined") {
-                chrome.tabs.create({ url: request.url, windowId: incognitoWins[0].id });
-            } else {
-                try {
-                    chrome.windows.create(
-                        { url: request.url
-                        , incognito: true
-                        // May not be able to use `state` as it is supported
-                        // only in Chrome 41 and beyond.
-                        , state: state
-                        }
-                    );
-                } catch(e) {
-                    console.log("Trying without `state`");
-                    chrome.windows.create(
-                        { url: request.url
-                        , incognito: true
-                        }
-                    );
-                }
-            }
-        });
+        openIncognito(request.url);
         sendResponse(true);
+    }
+);
+
+chrome.browserAction.onClicked.addListener(
+    function(tab) {
+        openIncognito(tab.url);
     }
 );
